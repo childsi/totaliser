d49d93d9a37be07b870e9740e0e4af647a4384f8
Patrick Sinclair <metade@Patrick.config>
2010-04-15 00:19:05 +0100

    Adding image

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require 'active_support'
 require 'sinatra'
 require 'sinatra/respond_to'
 require 'json'
+require 'scruffy'
 require 'open-uri'
+
+mime_type :png, 'image/png'
 
 Sinatra::Application.register Sinatra::RespondTo
 
@@ -12,12 +15,28 @@ def buyabrick
   data['details']['total_money']
 end
 
+def google_doc
+  key = 'tilwvsGv-HTvaBD29cvaGXw'
+  url = "http://spreadsheets.google.com/pub?key=#{key}&single=true&gid=1&output=csv"
+  data = open(url).read
+  data.to_i
+end
+
+def totaliser_image(total)
+  graph = Scruffy::Graph.new
+  graph.renderer = Scruffy::Renderers::Standard.new
+  graph.add :line, [total, 20_000]
+  filename = "tmp/#{Time.now.to_i}.png"
+  graph.render :to => filename, :as => 'png'
+  open(filename).read  
+end
+
 get('/') do
   redirect 'http://www.childsifoundation.org'
 end
 
 get('/total') do
-  total = buyabrick
+  total = buyabrick + google_doc
   details = { :total => total }
 
   headers['Cache-Control'] = 'public, max-age=300'
@@ -27,5 +46,6 @@ get('/total') do
     wants.xml { details.to_xml }
     wants.json { details.to_json }
     wants.yaml { details.to_yaml }
+    wants.png { totaliser_image(total) }
   end
 end
